06de0cc10c55437be1d00a6afa5190c7209b50be
Evan Jamaq <113580904+newbiema@users.noreply.github.com>
2024-02-05 15:33:31 +0700

    Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
 				<div class="sm:flex sm:items-center sm:justify-between">
 					<div class="flex mt-4 justify-center sm:mt-0">
 						<p className="text-[0.7rem] text-white opacity-70">
-							© {new Date().getFullYear()} Kelas XI TKJ 3 | Di Kelola Oleh Siswa TKJ 3
+							© {new Date().getFullYear()}Kenangan Kelas XII IPS 1 | Di Kelola Oleh Evan
 						</p>
 					</div>
 				</div>
